e257667975428885fadb57c9a587668d757f35f5
DxxxHxx <donghun1163@gmail.com>
2024-03-31 15:21:08 +0900

    Refactor:리프레시 범위  수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
 import NavBar from "./components/navbar/NavBar";
 import Router from "./components/router";
+import { useRefreshToken } from "@/service/auth/useRefreshToken";
+import { accessTokenState } from "@/store";
+import { useEffect } from "react";
+import { useRecoilValue } from "recoil";
 
 export default function App() {
+  const accessToken = useRecoilValue(accessTokenState);
+  const refresh = useRefreshToken();
+  useEffect(() => {
+    if (accessToken === "") {
+      refresh();
+    }
+  }, [refresh, accessToken]);
   return (
     <BrowserRouter>
       <NavBar />
